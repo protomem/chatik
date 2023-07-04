2de8a46cce5d005130b84595219cdd08c069fcb6
protomem <protomem0709@gmail.com>
2023-07-04 11:55:08 +0300

    feat(web): add page: not found

--- web/src/pages/not-found/NotFound.jsx
+++ web/src/pages/not-found/NotFound.jsx
@@ -1,7 +1,45 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export function NotFound() {
+  const nav = useNavigate();
+
+  const handleClick = () => {
+    nav("/", { replace: true });
+  };
+
   return (
-    <div>
-      <h1>Not Found</h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">Not Found</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container
+        sx={{
+          mt: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <Typography variant="h6" align="center">
+          404 Page not found
+        </Typography>
+
+        <Button onClick={handleClick} variant="outlined">
+          Go home
+        </Button>
+      </Container>
+    </Box>
   );
 }
